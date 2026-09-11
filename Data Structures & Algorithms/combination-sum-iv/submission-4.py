f94ab1bb56fc6c61d7124b6744821a8ec15d17ab
Jakub Kowalczuk <150016575+Ja-Kuba@users.noreply.github.com>
2026-09-11 17:10:25 +0200

    Add: combination-sum-iv - submission-4

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-4.py
@@ -0,0 +1,40 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        
+        # nums - [3,1,2]
+
+
+        """
+
+         3        1                   2     
+        3 1 2   3 1      2          3  1 2  
+                  3 1 2  3 1 2      ...        
+               
+
+        we can use bfs to early stop existing combos
+        """
+
+        cache = {}
+
+
+        def dfs(t) ->int:
+            if t in cache:
+                return cache[t]
+
+            if t == 0:
+                return 1
+            elif t < 0:
+                return 0
+
+            ret = 0
+            for n in nums:
+                ret+=dfs(t-n)
+            
+            cache[t] = ret
+            return ret
+
+        total = 0
+        total+=dfs(target)
+
+        return total
+
