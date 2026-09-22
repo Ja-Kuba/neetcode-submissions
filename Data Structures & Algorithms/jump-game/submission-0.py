fc2db074935b40ef97009ec26d499c1c3c9ab4e1
Jakub Kowalczuk <150016575+Ja-Kuba@users.noreply.github.com>
2026-09-22 14:43:51 +0200

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        reach = len(nums)-1
+
+
+        for i in range(len(nums)-2, -1, -1):
+            for j in range(nums[i]+1):
+                if i + j >= reach:
+                    reach = i
+                    break
+
+
+        return True if reach == 0 else False
+
+
+
+
+"""
+INPUT
+[int]: integer array nums
+ nums[i] - maximum jump length at that position
+
+RETURN:
+true if you can reach the last index starting from index 0
+
+
+dp[i]:bool - can reach end 
+
+dp[i] = True if any dp[i+j] where j in range(nums[i])
+
+dp[len(nums)] = True
+
+return dp[0]
+
+we only need to store smallest reachable index 
+
+"""
