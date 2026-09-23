7b798c692a7ddd515447befb1b172880fc04eaf3
Jakub Kowalczuk <150016575+Ja-Kuba@users.noreply.github.com>
2026-09-23 19:50:31 +0200

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        r = 0
+        for n in nums:
+            r = r ^ n
+
+        return r
+
+
+
+
+
+
+'''
+[int]: nums
+
+ Every integer appears twice except for one.
+
+ Return the integer that appears only once.
+
+'''
+
+
+"""
+     XOR
+1 1  0 
+1 0  1
+0 1  1
+0 0  0
+
+"""
