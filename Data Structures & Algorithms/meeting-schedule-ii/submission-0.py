ea698e148b3cd190b375244eb050a25c958d1371
Jakub Kowalczuk <150016575+Ja-Kuba@users.noreply.github.com>
2026-09-23 18:57:30 +0200

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.py
@@ -0,0 +1,47 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        inter = sorted(intervals, key=lambda x: x.start)
+
+        max_overlaps = 0
+        minh = []
+        for m in inter:
+            while minh and minh[0] <= m.start:
+                heapq.heappop(minh)
+
+            heapq.heappush(minh,m.end)
+            max_overlaps = max(max_overlaps, len(minh))
+            
+
+
+
+
+        return max_overlaps
+
+
+
+
+"""
+[Interval]: intervals
+    [[start_1,end_1],[start_2,end_2],...]
+
+minimum number of rooms required to schedule all meetings without any conflicts
+
+BASICLY:
+MAX(overlaping at onnce)
+
+for empty its 1 or 0?
+
+[0,3] [2,5] [4,6]
+deque = [3,5]  
+max =2 
+[0,6] [2,5] [4,6]
+
+"""
